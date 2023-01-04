648f1fca812a03ad63ef30d1217b0cf6b1167038
mail2chuka <mail2chuka@gmail.com>
2023-01-04 11:59:24 +0100

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+<<<<<<< Updated upstream
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+=======
+import App from './containers/App'
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <App />
+=======
+ <App/>
+>>>>>>> Stashed changes
   </React.StrictMode>
 );
 
@@ -15,3 +26,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+<<<<<<< Updated upstream
+=======
+
+ 
+>>>>>>> Stashed changes
